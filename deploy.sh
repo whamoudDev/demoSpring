a6d18f747d3fc8bf6adddfc97ba7d962ce003db3
whamoud <hamoudwalid57@gmail.com>
2023-05-16 09:40:53 +0200

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Mettre à jour le code source
+git pull
+
+# Construire le projet avec Maven
+bash mvnw package -P prod,sysadmin --settings /home/debian/.m2/settings.xml
+
+# Construire l'image Docker
+docker build --no-cache -t demospring .
+
+# Arreter le conteneur existant
+docker stop demospring
+
+# Supprimer le conteneur existant
+docker rm demospring
+
+# Lancer un nouveau conteneur
+docker run -d --net backend --ip 172.18.0.4 --name=demospring -p 8080:8080 demospring
